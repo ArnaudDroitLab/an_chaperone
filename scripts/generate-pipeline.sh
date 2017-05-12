06c46bbf6f3bf74e0f6d5059b03c58256014234c
Eric Fournier <ericfournier2@yahoo.ca>
2017-05-12 12:14:17 -0400

    Initial commit.

---
+++ scripts/generate-pipeline.sh
@@ -0,0 +1,13 @@
+export RAP_ID="eav-760-aa"
+
+mkdir -p output/pipeline
+
+$MUGQIC_PIPELINES_HOME/pipelines/chipseq/chipseq.py -s '1-15' \
+    -l debug \
+    -r raw/readset.txt \
+    -d raw/design.txt \
+    -o output/pipeline \
+    --config $MUGQIC_PIPELINES_HOME/pipelines/chipseq/chipseq.base.ini \
+        $MUGQIC_PIPELINES_HOME/pipelines/chipseq/chipseq.guillimin.ini \
+        $MUGQIC_PIPELINES_HOME/resources/genomes/config/Saccharomyces_cerevisiae.R64-1-1.ini \
+        input/chipseq.numpy.bug.ini        
